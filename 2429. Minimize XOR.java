c8cafe9fd019e3deb2b0c7d7377dad511c233ece
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-01-15 23:52:36 +0530

    2429. Minimize XOR

---
+++ 2429. Minimize XOR.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimizeXor(int num1, int num2) {
+        int setBits = Integer.bitCount(num2);
+        int bit = 31;
+        int res = 0;
+        while(bit>= 0 && setBits>0){
+            if((num1 & (1<<bit)) != 0){
+                res = res | (1 << bit);
+                setBits--;
+            }
+            bit--;
+        } 
+        bit = 0;
+        while(setBits>0 && bit<=32){
+            if((num1 & (1<<bit)) == 0){
+                res = res | (1 << bit);
+                setBits--;
+            }
+            bit++;
+
+        }
+        return res;
+    }
+}
