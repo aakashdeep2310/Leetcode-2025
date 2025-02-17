a134d8faa9f6bca6571033115db5cf028df58268
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-02-17 21:28:52 +0530

    1079. Letter Tile Possibilities

---
+++ Letter Tile Possibilities.java
@@ -0,0 +1,23 @@
+class Solution {
+    int len;
+    public int numTilePossibilities(String tiles) {
+        len = tiles.length();
+        int freq[] = new int[26];
+        for(int i=0;i<len;i++){
+            freq[tiles.charAt(i) - 'A']++;
+        }
+        int count = backtrack(freq);
+        return count;
+    }
+    public int backtrack(int freq[]){
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]==0) continue;
+            count++;
+            freq[i]--;
+            count += backtrack(freq);
+            freq[i]++;
+        }
+        return count;
+    }
+}
