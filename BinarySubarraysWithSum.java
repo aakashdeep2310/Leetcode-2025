ff58e60ce75aa1f54d360ece43799e538b4b8720
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-05-26 22:52:08 +0530

    930. BinarySubarraysWithSum.java

--- BinarySubarraysWithSum.java
+++ BinarySubarraysWithSum.java
@@ -1,18 +1,42 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
         
-        int n = nums.length;
-        int count = 0;
+        // int n = nums.length;
+        // int count = 0;
         
-        for(int i = 0; i<n; i++){
-            if(nums[i] == goal) count++;
-            int sum = nums[i];
-            for(int j = i+1; j<n; j++){
-                sum += nums[j];
-                if(sum == goal){
-                    count++;
-                }
+        // for(int i = 0; i<n; i++){
+        //     if(nums[i] == goal) count++;
+        //     int sum = nums[i];
+        //     for(int j = i+1; j<n; j++){
+        //         sum += nums[j];
+        //         if(sum == goal){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+        return subArray(nums, goal) - subArray(nums, goal -1);
+    }
+
+    public int subArray(int[] nums, int goal){
+        int n = nums.length;
+        int l = 0;
+        int r = 0;
+        int sum = 0;
+        int count = 0;
+
+        if(goal < 0) return 0;
+
+        while(r < n){
+            sum += nums[r];
+
+            while(sum > goal){
+                sum -= nums[l];
+                l++;
             }
+            count += (r-l+1);
+            r++;
         }
         return count;
     }
