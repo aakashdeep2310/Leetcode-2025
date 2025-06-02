484a284e2e5f4bbb0cc90fb3c00a463ace818a66
Aakash Deep <90546941+aakashdeep2310@users.noreply.github.com>
2025-06-02 20:54:35 +0530

    873. Length of Longest Fibonacci Subsequence.java

--- 873. Length of Longest Fibonacci Subsequence.java
+++ 873. Length of Longest Fibonacci Subsequence.java
@@ -5,6 +5,7 @@ class Solution {
         for (int num : arr) {
             numSet.add(num);
         }
+        
         int maxLen = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
